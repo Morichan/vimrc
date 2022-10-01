6c1ee9829d1304ea5f559d6b639d30608f5ffb2a
Morichan <nintendo698@gmail.com>
2022-10-02 03:34:34 +0900

    🍰 chore(rm): 旧スクリプトファイルの除去
    5c2aaf2fd65ce1f2af0a6656cbbe206fc9ddb535 コミットにより、通常のシェル
    スクリプトファイルが不要になったため除去する。

--- install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh -e
-
-cat << EOF >> ~/.vimrc
-
-" This is custom settings, for more details $(pwd)/README.md
-set runtimepath+=$(pwd)/rc
-let g:VIM_DOTFILES_ROOT_DIR='$(pwd)'
-runtime! init.vim
-EOF
