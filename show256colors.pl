86afb15d0022b70d540cad5598ddb397068fcb40
Morichan <morichan.keisuke.mori@gmail.com>
2025-03-24 00:52:52 +0900

    :recycle: refactor(show256colors): 読みやすさの修正
    - インデントをタブ文字からスペース4文字に変更する
    - forからforeachに変更する
    - foreach内の記法をrange式に変更する
    - 外部のcolor変数をforeach内の変数で代用する
    - 文字列生成の関数名を変更する
    - 文字列のqqをダブルクォーテーションに変更する

--- show256colors.pl
+++ show256colors.pl
@@ -7,18 +7,16 @@ my $fg = "\x1b[38;5;";
 my $bg = "\x1b[48;5;";
 my $rs = "\x1b[0m";
 
-my $color = 0;
-
-for (my $row = 0; $row < 32; ++$row) {
-	for (my $col = 0; $col < 8; ++$col) {
-		print get_color($color);
-		$color++;
-	}
-	print "\n";
+foreach my $row (0..31) {
+    foreach my $column (0..7) {
+        print generate_color_text($row * 8 + $column);
+    }
+    print "\n";
 }
 
-sub get_color {
-	my ($color) = @_;
-	my $number = sprintf '%3d', $color;
-	return qq/${bg}${color}m ${number} ${rs} ${fg}${color}m${number}${rs} /;
+sub generate_color_text {
+    my ($color) = @_;
+    my $number = sprintf '%3d', $color;
+    return "${bg}${color}m ${number} ${rs} ${fg}${color}m${number}${rs} ";
 }
+
